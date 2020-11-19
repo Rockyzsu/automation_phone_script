2b013e6ccad22775704487494af3e54f2583ae9f
Rocky Chen <weigesysu@qq.com>
2020-11-19 22:32:21 +0800

    update

--- stock/README.md
+++
@@ -1,2 +0,0 @@
-# autojs_stock
-使用autojs自动化交易股票（华宝油气自动申购等）
---
+++ stock/alipay_redbag.js
@@ -0,0 +1,50 @@
+// toast('starting...');
+// 根据id 非常难找控件
+
+/*
+if(id("bt_pass_action").exists()){
+    log('找到一个bt_pass_action')
+}
+else{
+    log('没找到bt_pass_action')
+}
+
+sleep(1000)
+if(text("立即使用").exists()){
+    log('立即使用')
+}
+else{
+
+    log('没有找到立即使用')
+}
+sleep(1000)
+*/
+
+
+
+// right_top_btn = id("right_text").find();
+// log('start')
+// log(right_top_btn)
+// log('end')
+// toast('button');
+// toast(right_top_btn);
+// click(right_top_btn.bounds().centerX(),right_top_btn.bounds().centerY())
+// sleep(500)
+// delete_btn = text('删除').findOne()
+// delete_btn.click()
+// sleep(500)
+// confirm_btn = text('确定').findOne()
+// confirm_btn.click()
+// sleep(500)
+
+
+// if(className('android.widget.FrameLayout').exists())
+// {
+//     log('找到了');
+
+//     layout = className('android.widget.FrameLayout').find();
+//     toast(length(layout));
+//     // log('end');
+// }
+// let btn = desc('更多操作').findOnce(2000)
+// btn.click()
